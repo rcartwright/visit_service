bdb8ca56d2459990855eb2aab5aca4da758f7810
Rachel D. Cartwright <stormchica@gmail.com>
2022-06-20 15:39:03 -0500

    make user list test work

--- server/visit/config/test.exs
+++ server/visit/config/test.exs
@@ -9,13 +9,13 @@ config :visit, Visit.Repo,
   username: "postgres",
   password: "postgres",
   database: "visit_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
+  hostname: System.get_env("PGHOST"),
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :visit, VisitWeb.Endpoint,
-  http: [port: 4002],
+  http: [port: 4003],
   server: false
 
 # Print only warnings and errors during test
