1660ae7a31ca3fb970b9d93ae908c5e0ca9d37e2
Rachel D. Cartwright <stormchica@gmail.com>
2022-06-22 00:53:50 -0500

    add notes

--- server/visit_api/lib/visit_api_web/controllers/visit_controller.ex
+++ server/visit_api/lib/visit_api_web/controllers/visit_controller.ex
@@ -11,9 +11,25 @@ defmodule VisitApiWeb.VisitController do
     render(conn, "index.json", visits: visits)
   end
 
+  @doc """
+  Sample request body for create
+  {
+    "visit": {
+        "member_user_id": "some member_user_id",
+        "minutes": 43,
+        "requested_on": "2011-05-18T15:01:01Z",
+        "visit_date": "2011-05-18T15:01:01Z"
+    }
+  }
+  """
+
+  # change this to be called request_visit instead in the future to be more specific
   def create(conn, %{"visit" => visit_params}) do
     with {:ok, %Visit{} = visit} <- Visits.create_visit(visit_params) do
       conn
+      # look up user from passed in member_user_id - in the future - change to be a bearer token
+      # select id, minute_total from Users where member_user_id = member_user_id
+      # check to make sure user has enough minutes left & that user doesn't already have anything already submitted for that day
       |> put_status(:created)
       |> put_resp_header("location", Routes.visit_path(conn, :show, visit))
       |> render("show.json", visit: visit)
