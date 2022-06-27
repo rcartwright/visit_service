87e6a9ee82665a75f556e0bd239d8abc8bd580c1
Rachel D. Cartwright <stormchica@gmail.com>
2022-06-26 19:31:45 -0500

    some cleanup

--- server/visit_api/test/visit_api_web/controllers/visit_controller_test.exs
+++ server/visit_api/test/visit_api_web/controllers/visit_controller_test.exs
@@ -7,19 +7,19 @@ defmodule VisitApiWeb.VisitControllerTest do
   alias VisitApi.Accounts.User
   alias VisitApi.Accounts
 
-  @create_attrs %{
+  @create_visit_attrs %{
     member_user_id: "some member_user_id",
     minutes: 42,
     requested_on: "2010-04-17T14:00:00Z",
     visit_date: "2010-04-17T14:00:00Z"
   }
-  @update_attrs %{
+  @update_visit_attrs %{
     member_user_id: 3,
     minutes: 43,
     requested_on: "2011-05-18T15:01:01Z",
     visit_date: "2011-05-18T15:01:01Z"
   }
-  @invalid_attrs %{member_user_id: nil, minutes: nil, requested_on: nil, visit_date: nil}
+  @invalid_visit_attrs %{member_user_id: nil, minutes: nil, requested_on: nil, visit_date: nil}
 
   @create_user_attrs %{
     email: "some email",
@@ -28,16 +28,14 @@ defmodule VisitApiWeb.VisitControllerTest do
     minutes_balance: 40
   }
 
-  def fixture(:visit) do
-    {:ok, visit} = Visits.create_visit(@create_attrs)
-    visit
-  end
-
   def fixture(:user) do
     {:ok, user} = Accounts.create_user(@create_user_attrs)
-    # IO.inspect("user inside fixture")
-    # IO.inspect(user)
     user
+  end
+
+  def fixture(:visit) do
+    {:ok, visit} = Visits.create_visit(@create_visit_attrs)
+    visit
   end
 
   setup %{conn: conn} do
@@ -54,51 +52,19 @@ defmodule VisitApiWeb.VisitControllerTest do
   describe "create visit" do
     setup [:create_user]
 
-    # test "renders visit when data is valid", %{conn: conn} do
-    #   conn = post(conn, Routes.visit_path(conn, :create), visit: @create_attrs)
-    #   assert %{"id" => uid} = json_response(conn, 201)["data"]
+    test "renders errors user does not have enough minutes to request visit", %{conn: conn, user: %User{id: user_id} = user} do
+      create_attrs = %{
+        member_user_id: user_id,
+        minutes: 50,
+        requested_on: "2010-04-17T14:00:00Z",
+        visit_date: "2010-04-17T14:00:00Z"
+      }
 
-    #   conn = get(conn, Routes.visit_path(conn, :show, uid))
+      conn = post(conn, Routes.visit_path(conn, :create), visit: create_attrs)
 
-    #   assert %{
-    #     "id" => uid,
-    #     "member_user_id" => "some member_user_id",
-    #     "minutes" => 42,
-    #     "requested_on" => "2010-04-17T14:00:00Z",
-    #     "visit_date" => "2010-04-17T14:00:00Z"
-    #   } = json_response(conn, 200)["data"]
-    # end
+      assert json_response(conn, 422)["errors"] != %{}
+    end
 
-
-    # test "renders errors user does not have enough minutes to request visit", %{conn: conn, user: %User{id: id} = user} do
-    #   create_attrs = %{
-    #     member_user_id: id,
-    #     minutes: 50,
-    #     requested_on: "2010-04-17T14:00:00Z",
-    #     visit_date: "2010-04-17T14:00:00Z"
-    #   }
-
-    #   conn = post(conn, Routes.visit_path(conn, :create), visit: create_attrs)
-
-    #   assert json_response(conn, 422)["errors"] != %{}
-    # end
-
-
-    # test "renders visit when data is valid", %{conn: conn} do
-    #   conn = post(conn, Routes.visit_path(conn, :create), visit: @create_attrs)
-    #   assert %{"id" => id} = json_response(conn, 201)["data"]
-
-    #   conn = get(conn, Routes.visit_path(conn, :show, id))
-
-    #   assert %{
-    #            "id" => id,
-    #            "member_user_id" => "some member_user_id",
-    #            "minutes" => 42,
-    #            "requested_on" => "2010-04-17T14:00:00Z",
-    #            "visit_date" => "2010-04-17T14:00:00Z"
-    #          } = json_response(conn, 200)["data"]
-    # end
-    @tag mustexec: true
     test "renders visit when data is valid", %{conn: conn, user: %User{id: user_id} = user} do
       create_attrs = %{
         member_user_id: user_id,
@@ -120,17 +86,18 @@ defmodule VisitApiWeb.VisitControllerTest do
       } = json_response(conn, 200)["data"]
 
     end
-    # test "renders errors when data is invalid", %{conn: conn} do
-    #   conn = post(conn, Routes.visit_path(conn, :create), visit: @invalid_attrs)
-    #   assert json_response(conn, 422)["errors"] != %{}
-    # end
+
+    test "renders errors when data is invalid", %{conn: conn} do
+      conn = post(conn, Routes.visit_path(conn, :create), visit: @invalid_visit_attrs)
+      assert json_response(conn, 422)["errors"] != %{}
+    end
   end
 
   # describe "update visit" do
   #   setup [:create_visit]
 
   #   test "renders visit when data is valid", %{conn: conn, visit: %Visit{id: id} = visit} do
-  #     conn = put(conn, Routes.visit_path(conn, :update, visit), visit: @update_attrs)
+  #     conn = put(conn, Routes.visit_path(conn, :update, visit), visit: @update_visit_attrs)
   #     assert %{"id" => ^id} = json_response(conn, 200)["data"]
 
   #     conn = get(conn, Routes.visit_path(conn, :show, id))
@@ -145,7 +112,7 @@ defmodule VisitApiWeb.VisitControllerTest do
   #   end
 
   #   test "renders errors when data is invalid", %{conn: conn, visit: visit} do
-  #     conn = put(conn, Routes.visit_path(conn, :update, visit), visit: @invalid_attrs)
+  #     conn = put(conn, Routes.visit_path(conn, :update, visit), visit: @invalid_visit_attrs)
   #     assert json_response(conn, 422)["errors"] != %{}
   #   end
   # end
