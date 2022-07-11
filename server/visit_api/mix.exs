737ac42b6cbac67f280f290c44ae98ab398dba5d
Rachel D. Cartwright <stormchica@gmail.com>
2022-07-11 00:29:01 -0500

    fix dependency

--- server/visit_api/mix.exs
+++ server/visit_api/mix.exs
@@ -45,7 +45,7 @@ defmodule VisitApi.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      { :uuid, "~> 1.1", only: :test }
+      { :uuid, "~> 1.1" }
     ]
   end
 
